e15c593af408fd68407282f0f8bc5d7c187c13ff
kts123 <kts123@estsoft.com>
2020-12-28 23:45:38 +0900

    fps=5

--- predict.py
+++ predict.py
@@ -18,8 +18,8 @@ config_file     = f'{BASE}/weights/v4/test_config.py'
 checkpoint_file = f'{BASE}/weights/v4/epoch_27.pth'
 
 TOP_N = 10 
-FPS = 10 
-MIN_SIZE = 20 # 규정은 32px 이지만 안전을 위해 30으로 설정
+FPS = 5 
+MIN_SIZE = 25 # 규정은 32px 이지만 안전을 위해 30으로 설정
 
 CONF_TH = 0.0
 
