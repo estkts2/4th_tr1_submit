1e72056830aa01dda50910db531f95a21848db37
kts123 <kts123@estsoft.com>
2020-12-28 23:18:37 +0900

    code refactoring

--- launch_tool.py
+++ launch_tool.py
@@ -1,26 +1,12 @@
 import os
 
-def get_free_port_():
-    try:
-        import socket
-        from contextlib import closing
-        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
-            s.bind(('', 0))
-            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            return s.getsockname()[1]
-    except:
-        return 45312
-    
 def run_test(config_file, checkpoint_file,
-             out_pickle_path, num_gpu=1):
+             out_pickle_path, gpu_order=0):
     
-    port = get_free_port_()
-    
-    cmd = f"""python -m torch.distributed.launch --nproc_per_node={num_gpu} --master_port={port}\
+    cmd = f"""CUDA_VISIBLE_DEVICES={gpu_order} python \
     /root/VarifocalNet/tools/test.py \
     --out {out_pickle_path} \
     --format-only \
-    --launcher pytorch \
     {config_file} {checkpoint_file} 
     """
     
